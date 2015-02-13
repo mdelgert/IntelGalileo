46cb5558e77ff8198cf59d3ff9c3953de40898bf
Matthew David Elgert <mdelgert@yahoo.com>
2015-02-13 02:15:29 -0700

    Added Intel Galileo SD Card Image

--- Sources/IntelGalileoSegfaultLibthreadFix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'Intel Galileo Seq Faults script fix for quark processor'
-
-for i in `find / -type f -name \*pthread\*so`; do sed -i "s/\xf0\x0f\xb1\x8b/\x90\x0f\xb1\x8b/g" ${i}; done
-
