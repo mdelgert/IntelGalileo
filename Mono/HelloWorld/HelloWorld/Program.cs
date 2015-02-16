a2977cb1aae65f6c75b2d67e83709539ef6820a7
Matthew David Elgert <mdelgert@yahoo.com>
2015-02-16 16:21:01 -0700

    Added Mono HelloWorld.exe example

---
+++ Mono/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a message was writtin in C#.");
+            Console.WriteLine("Mono runs .Net applications in Linux.");
+            Console.WriteLine("Press any key to exit.....");
+            Console.ReadKey();
+        }
+    }
+}
+
+//Reference articles
+//http://www.mono-project.com/docs/getting-started/install/linux/#debian-ubuntu-and-derivatives
+//http://www.mono-project.com/docs/getting-started/mono-basics
