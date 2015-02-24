0ee4eeb35a5015e078c25a823ee8181d37dc9b99
Matthew David Elgert <mdelgert@yahoo.com>
2015-02-24 00:37:46 -0700

    added mono MVCTest

---
+++ Scripts/Rsync2.sh
@@ -0,0 +1,5 @@
+rsync -aq /src /dest
+
+Copy files in Linux, avoid the copy if files do exist in destination - http://superuser.com/questions/118781/copy-files-in-linux-avoid-the-copy-if-files-do-exist-in-destination
+
+rsync -aqv /Data/mdelgert root@NAS2:/mnt/array1/Data/NAS1
